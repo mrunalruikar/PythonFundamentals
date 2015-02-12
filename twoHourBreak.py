06b19b71ef859a0c46f91785df96c59d5240097b
Mrunal Ruikar <mrunalruikar@gmail.com>
2015-02-12 15:57:09 -0600

    twoHourBreak

---
+++ twoHourBreak.py
@@ -0,0 +1,11 @@
+__author__ = 'MR028042'
+import webbrowser
+import time
+
+loopCnt = 0
+
+while loopCnt < 4:
+
+    webbrowser.open("http://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    time.sleep(100)
+    loopCnt += 1
